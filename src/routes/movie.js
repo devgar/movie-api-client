4301ac28cb642ecd242bf480e053b46694b92293
Edgar Albalate <dev.gardo@gmail.com>
2022-07-31 21:12:02 +0000

    Basic movie page with param identification

--- src/routes/movie.js
+++ src/routes/movie.js
@@ -1,9 +1,13 @@
+import { useParams } from 'react-router-dom'
+
 export default function Movie() {
+	const params = useParams()
+
 	return (
 		<main>
-			<h1>Movie ???</h1>
+			<h1>Movie # {params.movieId}</h1>
 			<p>
-				This is the movie page for ???.
+				This is the movie page for # {params.movieId}
 			</p>
 		</main>
 	)
