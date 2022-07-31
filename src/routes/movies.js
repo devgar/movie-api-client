9ae5800219efa5da1d758c213dc8aef2a881d70e
Edgar Albalate <dev.gardo@gmail.com>
2022-07-31 21:05:09 +0000

    Starts listing movies

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,11 +1,29 @@
+import { Link, Outlet } from 'react-router-dom'
+
+import { getPopularMovies } from "../data"
 
 export default function Movies() {
+	let movies = getPopularMovies()
 	return (
-		<main>
-			<h1>Movies</h1>
-			<p>
-				This is the movies page.
-			</p>
-		</main>
+		<section style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+					flexGrow: 1,
+        }}
+      >
+        {movies.map((movie) => (
+          <Link
+            style={{ display: "block", margin: "1rem 0" }}
+            to={`/movies/${movie.id}`}
+            key={movie.id}
+          >
+            {movie.title}
+          </Link>
+        ))}
+      </nav>
+			<Outlet />
+    </section>
 	)
 }
